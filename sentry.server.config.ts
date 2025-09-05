130eca3d18eafc0d43e8462236d757d3c7dbf9f0
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2025-09-04 23:13:18 -0400

    Add Sentry console logging integration to edge and server configurations

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -15,4 +15,9 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  
+  integrations: [
+  // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
 });
