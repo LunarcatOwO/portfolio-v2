11d6f4cd7db9b56e38d7adaa4325c9a49a4d768d
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2025-09-04 22:42:04 -0400

    Update page component with WIP message and redirect link

--- app/page.tsx
+++ app/page.tsx
@@ -17,5 +17,5 @@
 import Image from "next/image";
 
 export default function Page() {
-  return <main></main>;
+  return (<text>Oh. You found me... This is still WIP please visit https://old.lunarcatowo.space for now</text>)
 }
