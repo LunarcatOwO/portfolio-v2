77b63962db3538a0e7421a3d2758530feeffc0e6
LunarcatOwO <54731257+LunarcatOwO@users.noreply.github.com>
2025-09-08 10:33:45 -0400

    did organizing of files

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 "use client";
-import TechCardSection from "../components/TechCard";
-import ProjectCard from "../components/ProjectCard";
+import TechCardSection from "../components/home/TechCard";
+import ProjectCard from "../components/home/ProjectCard";
 import ProfilePicture from "../components/ProfilePicture";
 import { siGithub, siNodedotjs, siNextdotjs, siDocker, siGit, siTypescript } from "simple-icons";
 import { Zap, Coffee } from "lucide-react";
